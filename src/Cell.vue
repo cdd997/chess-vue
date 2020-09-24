40e8f0840d6b0bae9e36320111bd0ab14b6ecef0
陈建平 <39631309+cdd997@users.noreply.github.com>
2020-09-24 17:11:00 +0800

    2

---
+++ src/Cell.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="cell" v-on:click="onClickMe">
+      <template v-if="a">{{text}}</template>
+      <template v-else></template>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["n", "finished"],
+    data: () => {
+      return {
+        a: false,
+        text: '',
+      };
+    },
+    methods: {
+      onClickMe() {
+        if (this.text !== '') {
+          return;
+        }
+        this.a = true;
+        this.text = this.n % 2 == 0 ? 'X' : 'O';
+        this.$emit('click', this.text);
+      }
+    }
+  };
+</script>
+
+<style>
+  .cell {
+    border: 1px solid black;
+    width: 100px;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 80px;
+  }
+</style>
